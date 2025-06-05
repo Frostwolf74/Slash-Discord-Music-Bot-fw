c2d57d21bc2d1e5d1dfecab00e44c1ca629f7452
Frost <152447084+Frostwolf74@users.noreply.github.com>
2025-06-04 21:41:23 -0600

    removed debug statement

--- Utils.py
+++ Utils.py
@@ -571,10 +571,6 @@ class Pretests:
             cursor = db.cursor()
 
             cursor.execute("""
-                UPDATE Updates SET update_day = 3
-            """)
-
-            cursor.execute("""
                 SELECT update_day FROM Updates;
             """)
 
